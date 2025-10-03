906d8bba5692f0da567a1f13594d41d6b73aa211
Achuth Dintakurthi <90602876+Achuth-AA@users.noreply.github.com>
2025-10-03 15:34:40 +0000

    Adding the Boiler Template

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function LandingPage() {
+  return (
+    <div>
+      <h1>Welcome to Blue Chess</h1>
+      <p>A modern, feature-rich online chess platform</p>
+    </div>
+  );
+}
+
+export default LandingPage;
